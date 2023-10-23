e1fa6f17fdb2213d3aed18332697159d59101777
DESKTOP-5TJ2NI3\csi22-mdiarro <csi22-mdiarro@colegioaltair.net>
2023-10-23 14:04:01 +0200

    Ejercicio 1 Condicionales

---
+++ Controladores/Program.cs
@@ -0,0 +1,23 @@
+﻿using Ejercicio1Condicionales.Servicios;
+
+namespace Ejercicio1Condicionales.Controladores
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            NumeroInterfaz ni = new NumeroImplementacion();
+            int num;
+            num = ni.pedirNumero();
+            
+            if (num%2==0)
+            {
+                Console.WriteLine("El número {0} es un número par", num);
+            }
+            else
+            {
+                Console.WriteLine("El número {0} es un número impar", num);
+            }
+        }
+    }
+}

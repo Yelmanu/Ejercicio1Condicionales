409d23df2127d454d245eb3e7e3d3da59a1f0c2a
DESKTOP-5TJ2NI3\csi22-mdiarro <csi22-mdiarro@colegioaltair.net>
2023-10-23 14:13:43 +0200

    Ejercicio 1 Condicionales

--- Controladores/Program.cs
+++ Controladores/Program.cs
@@ -6,10 +6,14 @@ namespace Ejercicio1Condicionales.Controladores
     {
         static public void Main(string[] args)
         {
+            //Instanciamos la clase donde se encuentra el método a usar
             NumeroInterfaz ni = new NumeroImplementacion();
+
+            //Declaramos las variables
             int num;
             num = ni.pedirNumero();
             
+            //Realizamos la comprobación si el número es par o impar tal como pide el ejercicio
             if (num%2==0)
             {
                 Console.WriteLine("El número {0} es un número par", num);
